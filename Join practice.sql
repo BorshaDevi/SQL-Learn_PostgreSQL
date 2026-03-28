d1010ebb912268ad60bd001c7ee898176b884aec
Borsha Devi <borshadevi27@gmail.com>
2026-03-28 11:11:33 +0600

    practice some join in join practice.sql file.

--- Join practice.sql
+++ Join practice.sql
@@ -25,3 +25,33 @@ FROM film_actor
 ORDER BY
 film_id
 
+-- ch-1
+SELECT 
+* 
+FROM orders o INNER JOIN customer c ON o.order_id=c.customer_id
+SELECT * FROM orders
+
+-- ch-2
+SELECT 
+*
+FROM customer c RIGHT JOIN orders o ON c.customer_id = o.order_id
+WHERE c.customer_id IS null
+
+SELECT * FROM customer
+SELECT * FROM orders
+
+-- ch-3
+SELECT 
+o.order_id,c.customer_email 
+FROM orders o LEFT JOIN customer c ON o.order_id = c.customer_id 
+
+-- ch-4
+SELECT
+c.customer_id,
+SUM(p.amount) total_sum
+FROM customer c LEFT JOIN  payment p ON c.customer_id =p.customer_id
+GROUP BY
+c.customer_id 
+ORDER BY
+c.customer_id 
+
